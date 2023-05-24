718b5ad4f83520bc7009ac51da915668412f5ba9
SeoYun <rockin99kr@gmail.com>
2023-05-24 16:50:36 +0900

    add commit urls.py 0524 1650

--- Django_02/todo/urls.py
+++ Django_02/todo/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('post/', views.todo_post, name='todo_post'),
     path('<int:pk>/edit/', views.todo_edit, name='todo_edit'),
     path('done/', views.done_list, name='done_list'),
-    path('done/<int:pk>/', views.todo_done, name='todo_done')
+    path('done/<int:pk>/', views.todo_done, name='todo_done'),
 ]
